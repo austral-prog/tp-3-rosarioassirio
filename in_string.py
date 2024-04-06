5ab72729e9196e992300d983bb1d7020c597dfc6
rosarioassirio <162721687+rosarioassirio@users.noreply.github.com>
2024-04-06 13:21:17 -0300

    in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,16 @@
 def check_vowels():
+    nombre = input("Ingresa tu nombre")
+    a = "a" in name.lower()
+    b = "e" in name.lower()
+    c = "i" in name.lower()
+    d = "o" in name.lower()
+    e = "u" in name.lower()
+    print (f"Contiene a: {a}")
+    print (f"Contiene e: {b}")
+    print (f"Contiene i: {c}")
+    print (f"Contiene o: {d}")
+    print (f"Contiene u: {e}")
+check_vowels()
     # Código a implementar utilizando input.
 
 
